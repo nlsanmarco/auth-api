794dee4016802fc804ea297a3f2962c6e21f726e
Nicole Sanmarco <nicole.sanmarco@yahoo.com>
2021-09-10 18:38:41 -0400

    initial commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+  def create
+    user = User.new(
+      name: params[:name],
+      email: params[:email],
+      password: params[:password],
+      password_confirmation: params[:password_confirmation],
+    )
+    if user.save
+      render json: { message: "User created successfully" }, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :bad_request
+    end
+  end
+
+  def show
+    user = User.find(current_user.id)
+    render json: user.as_json
+  end
+end
